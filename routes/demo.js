bf5c123acb68f7c8fee26d492480bc664bc7cc41
davidheweb <davidhermosilladuro@gmail.com>
2022-08-01 10:08:17 +0200

    Adding sessions support to the website
    - Storing authentication data in sessions
    - Using sessions & cookies for controlling access
    - Adding the logout functionality
    - Adding a basic signup functionality and validating signup information
    - Hashing passwords
    - Adding user login functionality

--- routes/demo.js
+++ routes/demo.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 
 const db = require("../data/database");
 
@@ -16,14 +17,90 @@ router.get("/login", function (req, res) {
   res.render("login");
 });
 
-router.post("/signup", async function (req, res) {});
+router.post("/signup", async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const enteredConfirmEmail = userData["confirm-email"];
+  const enteredPassword = userData.password;
 
-router.post("/login", async function (req, res) {});
+  if (
+    !enteredEmail ||
+    !enteredConfirmEmail ||
+    !enteredPassword ||
+    enteredPassword.trim() < 6 ||
+    enteredEmail !== enteredConfirmEmail ||
+    !enteredEmail.includes("@")
+  ) {
+    console.log("Incorrect data");
+    return res.redirect("/signup");
+  }
+
+  const existingUser = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: enteredEmail });
+
+  if (existingUser) {
+    console.log("User exists already");
+    return res.redirect("/signup");
+  }
+
+  const hashedPassword = await bcrypt.hash(enteredPassword, 12);
+
+  const user = {
+    email: enteredEmail,
+    password: hashedPassword,
+  };
+
+  await db.getDb().collection("users").insertOne(user);
+
+  res.redirect("/login");
+});
+
+router.post("/login", async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const enteredPassword = userData.password;
+
+  const existingUser = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: enteredEmail });
+
+  if (!existingUser) {
+    console.log("Could not log in!");
+    return res.redirect("/login");
+  }
+
+  const passwordsAreEqual = await bcrypt.compare(
+    enteredPassword,
+    existingUser.password
+  );
+
+  if (!passwordsAreEqual) {
+    console.log("Could not log in - passwords are not equal!");
+    return res.redirect("/login");
+  }
+
+  req.session.user = { id: existingUser._id, email: existingUser.email };
+  req.session.isAuthenticated = true;
+  req.session.save(function () {
+    res.redirect("/admin");
+  }); // forcing the save, since the automatic save is asynchronous and when redirecting to a protected page, the redirection may be executed first
+});
 
 router.get("/admin", function (req, res) {
+  if (!req.session.isAuthenticated) {
+    // if (!req.session.user)
+    return res.status(401).render("401");
+  }
   res.render("admin");
 });
 
-router.post("/logout", function (req, res) {});
+router.post("/logout", function (req, res) {
+  req.session.user = null;
+  req.session.isAuthenticated = false;
+  res.redirect("/");
+});
 
 module.exports = router;
